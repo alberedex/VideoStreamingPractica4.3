3378b8b3a5a314a164829e8bed825987d110066b
Alberto Redondo <2001rs@gmail.com>
2023-03-13 22:08:56 +0100

    php form

--- writeJSONBackup.php
+++ writeJSONBackup.php
@@ -4,9 +4,9 @@ $json = $_POST['jsonObj'];
 
 $fecha = time();
 
-$campos = da
+$campos = getdate($fecha);
 
-$fd = fopen("Backup-.txt","a+") or die ("Error al abrir el archivo Ejemplo.txt");
+$fd = fopen("Backup-$campos[mday]/$campos[mon]/$campos[year]$campos[hours]:$campos[minutes].txt","a+") or die ("Error al abrir el archivo Ejemplo.txt");
 
 fputs($fd,$json);
 
